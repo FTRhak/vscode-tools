ffb48956b3b1e709cc2099ef9a111cbebe247496
taras.pylat <taras.pylat@eleks.com>
2026-09-14 09:42:01 +0300

    added generate-environment

--- src/angular.ts
+++ src/angular.ts
@@ -4,6 +4,7 @@ import { angularCommandGenerateApplication } from './angular/generate-applicatio
 import { angularCommandGenerateClass } from './angular/generate-class';
 import { angularCommandGenerateComponent } from './angular/generate-component';
 import { angularCommandGenerateDirective } from './angular/generate-directive';
+import { angularCommandGenerateEnvironment } from './angular/generate-environment';
 import { angularCommandGenerateModule } from './angular/generate-module';
 import { angularCommandGeneratePipe } from './angular/generate-pipe';
 import { angularCommandGenerateService } from './angular/generate-service';
@@ -18,6 +19,7 @@ export function angularCommands(context: vscode.ExtensionContext): vscode.Dispos
         ...angularCommandGenerateClass(context),
         ...angularCommandGenerateComponent(context),
         ...angularCommandGenerateDirective(context),
+        ...angularCommandGenerateEnvironment(context),
         ...angularCommandGenerateModule(context),
         ...angularCommandGeneratePipe(context),
         ...angularCommandGenerateService(context)
---
+++ src/angular/generate-environment.ts
@@ -0,0 +1,126 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as vscode from "vscode";
+
+import { PathElement, ProjectElement, TypeElement } from "../form_elements";
+
+export function angularCommandGenerateEnvironment(
+  context: vscode.ExtensionContext,
+): vscode.Disposable[] {
+  let commands: vscode.Disposable[] = [];
+
+  // Command to create Angular environment files
+  commands.push(
+    vscode.commands.registerCommand(
+      "vscode-angular.createAngularEnvironment",
+      (resource: vscode.Uri) => {
+        let absolutePath = resource.fsPath;
+        const stats = fs.statSync(absolutePath);
+        if (!stats.isDirectory()) {
+          absolutePath = path.dirname(absolutePath);
+        }
+        const workspaceFolder = vscode.workspace.getWorkspaceFolder(resource);
+        let relativePath = absolutePath;
+
+        if (workspaceFolder) {
+          relativePath = path.relative(
+            workspaceFolder.uri.fsPath,
+            absolutePath,
+          );
+        }
+
+        const panel = vscode.window.createWebviewPanel(
+          "formPageGenerateAngularEnvironment", // internal ID
+          "Angular Generate Environments", // tab title
+          vscode.ViewColumn.One, // show in first column
+          { enableScripts: true }, // allow JS in the webview
+        );
+
+        // HTML content for the form
+        panel.webview.html = getWebviewContent(
+          panel,
+          context.extensionUri,
+          relativePath,
+        );
+
+        panel.webview.onDidReceiveMessage(
+          (message) => {
+            if (message.command === "angular-create-environment") {
+              vscode.window.showInformationMessage(
+                `Generating Environments${message.project ? `: ${message.project}` : ""}`,
+              );
+
+              let command = 'echo "Error Command"';
+              const flags = message.project
+                ? ` --project=${message.project}`
+                : "";
+
+              if (message.type === "ng") {
+                command = `ng generate environments` + flags;
+              } else if (message.type === "nx") {
+                command = `nx g @nx/angular:environments` + flags;
+              }
+
+              const terminal = vscode.window.createTerminal(
+                "Generation Terminal",
+              );
+              terminal.sendText(command);
+              terminal.show();
+            }
+          },
+          undefined,
+          context.subscriptions,
+        );
+      },
+    ),
+  );
+
+  return commands;
+}
+
+function getWebviewContent(
+  panel: any,
+  extensionUri: vscode.Uri,
+  pathUrl: string,
+): string {
+  const stylePath = vscode.Uri.joinPath(
+    extensionUri,
+    "dist",
+    "media",
+    "styles.css",
+  );
+  const styleUri = panel.webview.asWebviewUri(stylePath);
+
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Generate environments</title>
+  <link href="${styleUri}" rel="stylesheet">
+</head>
+<body class="vscode-angular">
+  <h2>Generate environments for:</h2>
+  <form id="myForm">
+    <input type="hidden" name="command" value="angular-create-environment">
+    ${PathElement(pathUrl)}
+    ${TypeElement()}
+    ${ProjectElement()}
+
+    <button type="submit" id="submitBtn" class="btn">Generate</button>
+  </form>
+
+  <script>
+    const vscode = acquireVsCodeApi();
+    document.getElementById('myForm').addEventListener('submit', (ev) => {
+      const formData = new FormData(ev.target);
+      const formObject = Object.fromEntries(formData.entries());
+      vscode.postMessage(formObject);
+      
+      ev.stopPropagation();
+      ev.preventDefault();
+      return false;
+    });
+  </script>
+</body>
+</html>`;
+}
